115e05089bdffeac3465f67026679074eaf383f8
Phuong Lam <phuonglam@kms-technology.com>
2019-08-12 14:53:00 +0700

    fix bug in empworkingtime

--- src/components/EmpWorkingTimeReport.vue
+++ src/components/EmpWorkingTimeReport.vue
@@ -108,9 +108,9 @@ export default {
         .then(response => {
           this.empWorkingTimeInfo = response.data;
         }, showErrorAlert)
-        .finally(() => {() =>
+        .finally(() => 
           store.commit("setLoading", false)
-        }
+        
           );
     }
   },
